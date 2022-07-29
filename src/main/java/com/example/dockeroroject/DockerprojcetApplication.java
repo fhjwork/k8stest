0f5717ebb39391d5234950de4d637580b067aac2
fhjwork <110165802+fhjwork@users.noreply.github.com>
2022-07-29 10:59:51 +0800

    Create DockerprojcetApplication.java

---
+++ src/main/java/com/example/dockeroroject/DockerprojcetApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerprojcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerprojcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerprojcetApplication.class, args);
+    }
+
+}
